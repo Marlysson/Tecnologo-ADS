cce089b9022f64599666c081ed6cb86168bea76e
Marlysson Silva <marlysson5@gmail.com>
2017-04-19 17:01:18 -0300

    Separando dados de inicialização de objetos em outro arquivo e importando-o nos testes.

---
+++ Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/fixtures.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+fixture_cliente = {
+    "cpf": "123.123.123-23",
+    "nome": "Marlysson",
+    "endereco": "Endereço 1",
+    "complemento": "Complemento 1",
+    "cidade": "Teresina",
+    "estado": "PI",
+    "cep": "64079-020",
+    "fone_residencial": "3232-3232",
+    "fone_trabalho": "1234-1234",
+    "renda_familiar": 2500,
+    "email": "marlysson5@gmail.com"
+}
+
+fixture_fornecedor = {
+    "cnpj": "22.333.444/4345-32",
+    "nome": "Armazém Paraíba",
+    "endereco": "Endereço 2",
+    "complemento": "Complemento 2",
+    "cidade": "São Paulo",
+    "estado": "SP",
+    "cep": "64077-044",
+    "fone": "3434-4444",
+    "responsavel": "José Fernandes",
+    "website": "http://www.armazemparaiba.com"
+}
+
+
+fixture_produto_livro = {
+    "nome_produto": "Não conte à ninguém",
+    "quantidade": 100,
+    "min_quantidade": 10
+}
+
+fixture_produto_cd = {
+    "nome_produto": "Skillet - Invincible",
+    "quantidade": 200,
+    "min_quantidade": 20
+}
+
--- Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/tests.py
+++ Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/tests.py
@@ -1,54 +1,13 @@
 
 from django.test import TestCase
 
-from model_mommy import mommy
+from .fixtures import *
 
 from .models import Cliente, Fornecedor, Produto, Pedido, ItemPedido, Venda,  \
     ItemVenda
 
 from datetime import date, timedelta
 import unittest
-
-
-fixture_cliente = {
-    "cpf": "123.123.123-23",
-    "nome": "Marlysson",
-    "endereco": "Endereço 1",
-    "complemento": "Complemento 1",
-    "cidade": "Teresina",
-    "estado": "PI",
-    "cep": "64079-020",
-    "fone_residencial": "3232-3232",
-    "fone_trabalho": "1234-1234",
-    "renda_familiar": 2500,
-    "email": "marlysson5@gmail.com"
-}
-
-fixture_fornecedor = {
-    "cnpj": "22.333.444/4345-32",
-    "nome": "Armazém Paraíba",
-    "endereco": "Endereço 2",
-    "complemento": "Complemento 2",
-    "cidade": "São Paulo",
-    "estado": "SP",
-    "cep": "64077-044",
-    "fone": "3434-4444",
-    "responsavel": "José Fernandes",
-    "website": "http://www.armazemparaiba.com"
-}
-
-
-fixture_produto_livro = {
-    "nome_produto": "Não conte à ninguém",
-    "quantidade": 100,
-    "min_quantidade": 10
-}
-
-fixture_produto_cd = {
-    "nome_produto": "Skillet - Invencible",
-    "quantidade": 200,
-    "min_quantidade": 20
-}
 
 
 class TestCliente(TestCase):
@@ -171,6 +130,6 @@ class TestCliente(TestCase):
         quantidade_itens_venda = ItemVenda.objects.filter(
             codigo_venda=venda).count()
 
-        self.assertEquals(quantidade_itens_venda,2)
+        self.assertEquals(quantidade_itens_venda, 2)
 
         self.assertEquals(550, venda.valor_total)
