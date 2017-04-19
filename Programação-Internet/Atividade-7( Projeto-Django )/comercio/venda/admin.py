e6f3884753d51ef8ef6435e72cfa3edf363ba6d3
Marlysson Silva <marlysson5@gmail.com>
2017-04-19 16:31:42 -0300

    Iniciando criação de classes de admin.

--- Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/admin.py
+++ Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cliente, Fornecedor, Produto, Pedido, ItemPedido, Venda
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ["cpf", "nome", "endereco",
+                    "cidade", "estado", "cep", "email"]
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ["cnpj", "nome", "cep", "fone", "website"]
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ["data_pedido", "data_recebimento",
+                    "preco_total", "codigo_fornecedor"]
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+	list_display = ["nome_produto","quantidade"]
