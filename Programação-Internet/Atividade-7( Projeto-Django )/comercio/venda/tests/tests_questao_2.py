d4f23eff2fbd8ab066eff52c69c6733a1622ed11
Marlysson Silva <marlysson5@gmail.com>
2017-04-22 21:48:49 -0300

    Concluindo testes para a segunda questão.

--- Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/tests/tests_questao_2.py
+++ Programação-Internet/Atividade-7( Projeto-Django )/comercio/venda/tests/tests_questao_2.py
@@ -2,9 +2,10 @@
 
 from django.test import TestCase
 from django.db.models import Sum
+from django.db.models import F
 
 from venda.fixtures import fixture_cliente, fixture_produto_cd, \
-    fixture_produto_livro , fixture_fornecedor
+    fixture_produto_livro, fixture_fornecedor
 
 from venda.models import Cliente, Fornecedor, Produto, Venda, ItemVenda, ItemPedido, Pedido
 
@@ -12,6 +13,9 @@ from datetime import date
 
 
 class TestsSegundaQuestao(TestCase):
+
+    def tearDown(self):
+        Produto.objects.all().delete()
 
     def test_deve_retornar_todos_os_clientes_cadastrados(self):
 
@@ -37,13 +41,13 @@ class TestsSegundaQuestao(TestCase):
             codigo_cliente=cliente)
 
         item_cd = ItemVenda.objects.create(
-        	codigo_venda=venda_1,
+            codigo_venda=venda_1,
             codigo_produto=produto_cd,
             preco_unitario=25,
             quantidade=3)
 
         item_livro = ItemVenda.objects.create(
-        	codigo_venda=venda_1,
+            codigo_venda=venda_1,
             codigo_produto=produto_cd,
             preco_unitario=20,
             quantidade=5)
@@ -56,13 +60,13 @@ class TestsSegundaQuestao(TestCase):
             codigo_cliente=cliente)
 
         item_cd_2 = ItemVenda.objects.create(
-        	codigo_venda=venda_2,
+            codigo_venda=venda_2,
             codigo_produto=produto_cd,
             preco_unitario=50,
             quantidade=5)
 
         item_livro_2 = ItemVenda.objects.create(
-        	codigo_venda=venda_2,
+            codigo_venda=venda_2,
             codigo_produto=produto_livro,
             preco_unitario=25,
             quantidade=2)
@@ -73,75 +77,98 @@ class TestsSegundaQuestao(TestCase):
 
         # Compra 1
         venda_1.refresh_from_db()
-        self.assertEquals(venda_1.valor_total,175)
+        self.assertEquals(venda_1.valor_total, 175)
 
-        #Compra 2
+        # Compra 2
         venda_2.refresh_from_db()
-        self.assertEquals(venda_2.valor_total,300)
+        self.assertEquals(venda_2.valor_total, 300)
 
-        valor_das_compras = sum(cliente.minhas_compras.all().values_list("valor_total",flat=True))
+        valor_das_compras = sum(
+            cliente.minhas_compras.all().values_list("valor_total", flat=True))
 
         self.assertEquals(valor_das_compras, 475)
 
     def test_deve_retornar_um_pedido(self):
 
-    	from datetime import date , timedelta
+        from datetime import date, timedelta
 
-    	fornecedor = Fornecedor.objects.create(**fixture_fornecedor)
-    	produto = Produto.objects.create(**fixture_produto_cd)
+        fornecedor = Fornecedor.objects.create(**fixture_fornecedor)
+        produto = Produto.objects.create(**fixture_produto_cd)
 
-    	pedido = Pedido.objects.create(
-    		data_pedido=date.today(),
-    		data_recebimento=date.today() + timedelta(days=2),
-    		preco_total=0,
-    		codigo_fornecedor=fornecedor
-    	)
+        pedido = Pedido.objects.create(
+            data_pedido=date.today(),
+            data_recebimento=date.today() + timedelta(days=2),
+            preco_total=0,
+            codigo_fornecedor=fornecedor
+        )
 
-    	item_pedido = ItemPedido.objects.create(
-    		codigo_pedido=pedido,
-    		codigo_produto=produto,
-    		preco_unitario=50,
-    		quantidade=2
-    	)
+        item_pedido = ItemPedido.objects.create(
+            codigo_pedido=pedido,
+            codigo_produto=produto,
+            preco_unitario=50,
+            quantidade=2
+        )
 
-    	pedido = Pedido.objects.get(pk=pedido.pk)
+        pedido = Pedido.objects.get(pk=pedido.pk)
 
-    	self.assertEquals(1,pedido.codigo)
+        self.assertEquals(1, pedido.codigo)
 
     def test_deve_todos_os_itens_de_um_pedido(self):
 
-    	from datetime import date , timedelta
+        from datetime import date, timedelta
 
-    	fornecedor = Fornecedor.objects.create(**fixture_fornecedor)
-    	cd = Produto.objects.create(**fixture_produto_cd)
-    	livro = Produto.objects.create(**fixture_produto_livro)
+        fornecedor = Fornecedor.objects.create(**fixture_fornecedor)
+        cd = Produto.objects.create(**fixture_produto_cd)
+        livro = Produto.objects.create(**fixture_produto_livro)
 
-    	pedido = Pedido.objects.create(
-    		data_pedido=date.today(),
-    		data_recebimento=date.today() + timedelta(days=2),
-    		preco_total=0,
-    		codigo_fornecedor=fornecedor
-    	)
+        pedido = Pedido.objects.create(
+            data_pedido=date.today(),
+            data_recebimento=date.today() + timedelta(days=2),
+            preco_total=0,
+            codigo_fornecedor=fornecedor
+        )
 
-    	livros = ItemPedido.objects.create(
-    		codigo_pedido=pedido,
-    		codigo_produto=livro,
-    		preco_unitario=50,
-    		quantidade=2
-    	)
+        livros = ItemPedido.objects.create(
+            codigo_pedido=pedido,
+            codigo_produto=livro,
+            preco_unitario=50,
+            quantidade=2
+        )
 
-    	cds = ItemPedido.objects.create(
-    		codigo_pedido=pedido,
-    		codigo_produto=cd,
-    		preco_unitario=40,
-    		quantidade=3
-    	)
+        cds = ItemPedido.objects.create(
+            codigo_pedido=pedido,
+            codigo_produto=cd,
+            preco_unitario=40,
+            quantidade=3
+        )
 
-    	itens = pedido.itens.all()
+        itens = pedido.itens.all()
 
-    	queryset_dos_itens = []
+        queryset_dos_itens = []
 
-    	queryset_dos_itens.append('<ItemPedido: Produto=(nome=Não conte à ninguém, estoque=100), total=100.00>')
-    	queryset_dos_itens.append('<ItemPedido: Produto=(nome=Skillet - Invincible, estoque=200), total=120.00>')
+        queryset_dos_itens.append('<ItemPedido: Produto=(nome=Não conte à ninguém, estoque=102), total=100.00>')
+        queryset_dos_itens.append('<ItemPedido: Produto=(nome=Skillet - Invincible, estoque=203), total=120.00>')
 
-    	self.assertQuerysetEqual(list(itens),queryset_dos_itens)
+        self.assertQuerysetEqual(list(itens), queryset_dos_itens)
+
+    def test_deve_retornar_produtos_que_atingiram_o_estoque_minimo(self):
+
+        livro = Produto.objects.create(**fixture_produto_livro)
+        cliente = Cliente.objects.create(**fixture_cliente)
+
+        venda = Venda.objects.create(
+            data_venda=date.today(),
+            valor_total=0,
+            codigo_cliente=cliente)
+
+        item_livro = ItemVenda.objects.create(
+            codigo_venda=venda,
+            codigo_produto=livro,
+            preco_unitario=20,
+            quantidade=90)
+
+        produtos_min_estoque = Produto.objects.filter(quantidade__lte=F("min_quantidade") )
+
+        produtos_retornados = ['<Produto: nome=Não conte à ninguém, estoque=10>']
+
+        self.assertQuerysetEqual(list(produtos_min_estoque),produtos_retornados)
