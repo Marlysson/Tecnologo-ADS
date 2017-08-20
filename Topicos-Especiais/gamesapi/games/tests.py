5c657a6fae40a992e995e486652236d24ecfdb25
Marlysson <marlysson5@gmail.com>
2017-08-19 22:32:21 -0300

    Adicioanando testes de inserção via serializer, cliente do django usando objetos python e requests usando json.

--- Topicos-Especiais/gamesapi/games/tests.py
+++ Topicos-Especiais/gamesapi/games/tests.py
@@ -2,53 +2,102 @@ from django.test import TestCase
 from django.test import Client
 from .models import Game
 from .serializers import GameSerializer
+from pytz import timezone
+from datetime import datetime
+import requests
+
+class TestSerializer(TestCase):
+
+    def setUp(self):
+        
+        self.python_object_content = {
+            "name":"GTA V",
+            "release_date":datetime.now(),
+            "game_category":"Action/Adventure"
+        }
+
+    def test_should_be_return_count_objects_created(self):
+
+        game_serializer = GameSerializer(data=self.python_object_content)
+
+        if game_serializer.is_valid():
+            game_serializer.save()
+
+        count_inserted = Game.objects.all().count()
+
+        self.assertEqual(1,count_inserted)
+
+    def test_should_be_return_object_created_by_post_request_using_serializer(self):
+
+        from datetime import datetime
+
+        self.python_object_content["name"] = "Watch Dogs"
+
+        game_serializer = GameSerializer(data=self.python_object_content)
+
+        if game_serializer.is_valid():
+            game_serializer.save()
+
+        games = Game.objects.all()
+
+        game = games.get(pk=1)
+
+        self.assertEqual(game.name,"Watch Dogs")
+
 
 class TestApi(TestCase):
 
-	def setUp(self):
-		self.client = Client()
+    def setUp(self):
+        self.client = Client()
 
-		self.api_root = "/games/"
-		self.api_resource = "/games/{}"
+        self.api_root = "/games/"
+        self.api_resource = "games/{}/"
 
-		self.python_object_content = {
-			"name":"GTA V",
-			"release_date":datetime.now(),
-			"game_category":"Action/Adventure"
-		}
+        self.default_content = {
+            "name":"Pro Evolution Soccer 2016",
+            "release_date": datetime.now().isoformat(),
+            "game_category": "Futebol"
+        }
 
-	def test_resources_should_return_empty_when_havent_data(self):
+    def test_resources_should_return_empty_when_havent_data(self):
 
-		content = self.client.get(self.api_root).data
+        content = self.client.get(self.api_root).data
 
-		self.assertEqual(content,[])
+        self.assertEqual(content,[])
 
-	def test_should_be_return_count_objects_created(self):
+    def test_should_be_return_404_error_when_request_a_not_found_object(self):
 
-		from datetime import datetime
+        url_object = self.api_resource.format(3)
 
-		game_serializer = GameSerializer(data=self.python_object_content)
+        request = self.client.get(url_object)
 
-		if game_serializer.is_valid():
-			game_serializer.save()
+        self.assertEqual(404,request.status_code)
 
-		count_inserted = Game.objects.all().count()
+    def test_should_return_object_created(self):
 
-		self.assertEqual(1,count_inserted)
+        response = self.client.post(self.api_root,self.default_content)
 
-	def test_should_be_return_object_created_by_post_request_using_serializer(self):
+        game = response.data
 
-		from datetime import datetime
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(1,game["id"])
+        self.assertEqual("Pro Evolution Soccer 2016", game["name"])
 
-		game_serializer = GameSerializer(data=self.python_object_content)
+    def test_should_return_object_created_by_requests_library(self):
 
-		if game_serializer.is_valid():
-			game_serializer.save()
+        import json
 
-		games = Game.objects.all()
+        url = "http://localhost:8000/games/"
 
-		game = games.get(pk=1)
+        content = json.dumps(self.default_content)
 
-		self.assertEqual(game.name,"Watch Dogs")
+        response = requests.post(
+                            url,
+                            data=content,
+                            headers={"Content-Type":"application/json"}
+                    )
 
-	
+        content = response.json()
+
+        self.assertEqual(201,response.status_code)
+        self.assertEqual("Pro Evolution Soccer 2016X",content["name"])
